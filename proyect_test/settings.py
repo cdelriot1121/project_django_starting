eae3df0ce9dee9a0a7f079673e1b51f9397d7d0e
cdelriot1121 <carandresdelrio1121@gmail.com>
2024-12-04 18:25:19 -0500

    update settings for db mysql

--- proyect_test/settings.py
+++ proyect_test/settings.py
@@ -76,10 +76,14 @@ WSGI_APPLICATION = 'proyect_test.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': {   
+        'ENGINE': 'django.db.backends.mysql',   
+        'NAME': 'djangotest',   
+        'USER': 'root',   
+        'PASSWORD': 'password',   
+        'HOST': '127.0.0.1',   
+        'PORT': '3306',            
+    }  
 }
 
 

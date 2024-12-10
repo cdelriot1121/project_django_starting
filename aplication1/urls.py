777a879e39e4d1b5d1643fea11988930249e3e0a
cdelriot1121 <carandresdelrio1121@gmail.com>
2024-12-09 20:49:38 -0500

    update

--- aplication1/urls.py
+++ aplication1/urls.py
@@ -5,4 +5,6 @@ from . import views
 #este urls aplication es opcional para crear el view y parametrizar el urls propia de la aplication
 urlpatterns = [ 
     path('', views.index, name=''), #por ahora no esta mapeado con ninguna funcion de vista
+    path('', views.holaDjango, name= 'hola'),
+    path('persona/', views.holaPersona, name = 'persona'),
 ] 

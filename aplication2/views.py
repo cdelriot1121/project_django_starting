a5ea5d12331130f5324127ad0a6e1dd154e5bff2
cdelriot1121 <carandresdelrio1121@gmail.com>
2024-12-17 14:07:38 -0500

    update project

--- aplication2/views.py
+++ aplication2/views.py
@@ -28,7 +28,7 @@ def login_view(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('home')
+                return redirect('persona')
     else:
         form = LoginForm()
     return render(request, 'login.html', {'form': form})
